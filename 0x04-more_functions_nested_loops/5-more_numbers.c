69ab6301c69baf6ec67764bee37d80a178fffc0f
OkechukwuMmesoma <ls>
2023-09-14 17:23:34 -0700

    work done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - printing more numb
+ * more_numbers - printing more numbers
  */
 void more_numbers(void)
 {
@@ -9,7 +9,7 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (i = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
