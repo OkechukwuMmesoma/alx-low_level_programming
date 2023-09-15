54529e62fbffd3d913500de8febd44c1154c9f99
OkechukwuMmesoma <ls>
2023-09-14 17:10:35 -0700

    C - More Nested Loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - mult 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the mult
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Project: 0x04, C - More Nested Loops
